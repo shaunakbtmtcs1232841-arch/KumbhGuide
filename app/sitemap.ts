b072ea36db3cdc53face4eaa581327b662d81736
Mahekl01 <mahekgdsc@gmail.com>
2026-05-06 12:04:33 +0530

    sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,54 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://kumbhnashikguide.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kumbhnashikguide.com/blog",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kumbhnashikguide.com/temples",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kumbhnashikguide.com/ghats",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kumbhnashikguide.com/travel",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kumbhnashikguide.com/updates",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kumbhnashikguide.com/blog/what-is-kumbh-mela",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kumbhnashikguide.com/blog/nashik-kumbh-mela-2027-dates",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kumbhnashikguide.com/blog/top-ghats-nashik",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kumbhnashikguide.com/blog/significance-of-godavari-river",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kumbhnashikguide.com/blog/samudra-manthan-origin-kumbh",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kumbhnashikguide.com/blog/nashik-kumbh-mela-2027-development-plan",
+      lastModified: new Date(),
+    },
+  ];
+}
