42f97f77fecd28aaa1fd269b42b63c3b7b43b856
Mahekl01 <mahekgdsc@gmail.com>
2026-05-12 23:12:04 +0530

    nextjsup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',   // THIS enables static export
+
+  trailingSlash: false,
+
   images: {
     unoptimized: true // needed for static export
   }
