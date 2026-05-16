6a54e3bcb716d37f65b87b251a5a77ce0cea4101
Mahekl01 <mahekgdsc@gmail.com>
2026-05-16 21:21:16 +0530

    updatepage1

--- app/updates/page.tsx
+++ app/updates/page.tsx
@@ -44,6 +44,31 @@ export default function UpdatesPage() {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
 
           {/* UPDATE CARD 1 */}
+
+          <Link href="/blog/nashik-kumbh-mela-2027-development-plan">
+
+  <div className="bg-white rounded-2xl shadow-lg p-6 border hover:shadow-2xl transition duration-300 hover:-translate-y-1 cursor-pointer h-full">
+
+    <p className="text-sm text-orange-600 font-semibold mb-2">
+      Infrastructure Update
+    </p>
+
+    <h3 className="text-2xl font-bold mb-3">
+      Nashik Kumbh Mela 2027 Development Plan: ₹25,000 Crore Projects Explained
+    </h3>
+
+    <p className="text-gray-600 mb-5">
+      Major infrastructure upgrades including roads, railway stations, airport development, ghats, sanitation systems, and transportation improvements are planned ahead of Nashik Kumbh Mela 2027.
+    </p>
+
+    <span className="text-orange-600 font-semibold">
+      Read Full Update →
+    </span>
+
+  </div>
+
+</Link>
+
           <Link href="/updates/crowd-management-security-preparations-kumbh-2027">
 
             <div className="bg-white rounded-2xl shadow-lg p-6 border hover:shadow-2xl transition duration-300 hover:-translate-y-1 cursor-pointer h-full">
